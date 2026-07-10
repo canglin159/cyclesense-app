d5997b2bc764555bb67bf7a073035e580657ac4d
canglin159 <canglin159@users.noreply.github.com>
2026-07-10 14:09:51 +0000

    Fix: strict false for missing env vars

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
         runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
     },
     kit: {
@@ -11,7 +12,7 @@ const config = {
             assets: 'build',
             fallback: 'index.html',
             precompress: false,
-            strict: true
+            strict: false
         })
     }
 };
