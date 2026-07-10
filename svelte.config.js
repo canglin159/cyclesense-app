e61cfc4f71e66f60423fe19f0f9e6f2913e10b6b
canglin159 <149966219+canglin159@users.noreply.github.com>
2026-07-10 07:42:56 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,19 @@
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterAuto from '@sveltejs/adapter-auto';
-
-const isCapacitor = process.env.IS_CAPACITOR === 'true';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		adapter: isCapacitor 
-			? adapterStatic({
-				pages: 'build',
-				assets: 'build',
-				fallback: 'index.html',
-				precompress: false,
-				strict: true
-			}) 
-			: adapterAuto()
-	}
+    compilerOptions: {
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
